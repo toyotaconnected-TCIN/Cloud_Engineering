576f2bac7ac9fe0dd2a155feca3437e51c3cacf9
Ram-Nihal-Dev <pallabothularamanjaneyulu1@gmail.com>
2025-01-21 10:34:04 +0000

    intial commit

---
+++ main.tf
@@ -0,0 +1,14 @@
+#tcin-canv-tkm-prod resource block
+
+resource "aws_organizations_account" "tcin-canv-tkm-prod" {
+    name = var.account_name_tcin-canv-tkm-prod
+    email = var.mail_id_tcin-canv-tkm-prod
+    parent_id = var.OU_id_tcin-canv-tkm-prod
+    role_name = "AwsOrganizationAccountAccess"
+    close_on_deletion = "true"
+}
+
+output "accounetid_tcin-canv-tkm-prod" {
+    value =  aws_organizations_account.tcin-canv-tkm-prod.id
+  
+}
---
+++ vaiable.tf
@@ -0,0 +1,16 @@
+#variables of tcin-canv-tkm-prod
+variable "account_name_tcin-canv-tkm-prod" {
+    description = "aws account name to be added to organization"
+    type = string
+} 
+
+variable "mail_id_tcin-canv-tkm-prod" {
+    description = "aws account root user mail address"
+    type = string
+  
+}
+
+variable "OU_id_tcin-canv-tkm-prod" {
+  description = "aws orgnaization ou unit id"
+  type = string
+}
